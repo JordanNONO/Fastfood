053eddbb261a774d2fe59c6895707add11031e91
JordanNONO <jordannono2245@gmail.com>
2023-11-30 06:52:15 +0100

    Add project files.

---
+++ Fastfood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fastfood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
